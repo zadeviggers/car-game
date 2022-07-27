45a91474d3b0cae9495a2e54bb2d050b2e53fe1c
Zade Viggers <zade@viggers.net>
2022-07-27 22:39:53 +1200

    Better movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Constants
-    private const float MaxSpeed = 50.0f, Acceleration = 800.5f, WheelSpeed = 10.0f;
+    private const float ColliderTorque = 3500.0f, RenderRotationSpeed = 1.0f;
 
     // Components
     private Rigidbody rb;
-    private GameObject[] wheels;
-    
+    public List<Wheel> wheels;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        wheels = GameObject.FindGameObjectsWithTag("Wheel");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // --------------
         // Movement
@@ -28,32 +27,42 @@ public class PlayerController : MonoBehaviour
         // Left or right movement
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        // If there is any input...
-        if (horizontalInput != 0)
+        // If the user isn't telling the car to move, stop it from moving
+        if (horizontalInput == 0)
+        {
+            foreach (Wheel wheel in wheels)
+            {
+                wheel.collider.motorTorque = 0;
+            }
+        }
+        else
         {
             // This will be multiplied by the acceleration to change the direction
-            int direction = 1;
+            int direction = -1;
+
             // If the movement is left, make the multiplier negative
-            if (horizontalInput < 0) direction = -1;
-            
-            // Create a force vector for the acceleration
-            Vector3 movementForce = new Vector3(Acceleration * direction, 1);
+            if (horizontalInput < 0) direction = 1;
 
-            // Apply the force
-            rb.AddForce(movementForce, ForceMode.Force);
+            // Energy to apply to the wheels
+            float motorTorque = ColliderTorque * direction;
 
-            if (rb.velocity.magnitude > MaxSpeed)
+            // Rotation to make the wheels visually turn
+            Vector3 rotation = new Vector3(RenderRotationSpeed * direction, 0, 0);
+
+            // Move the car
+            foreach (Wheel wheel in wheels)
             {
-                rb.velocity = rb.velocity.normalized * MaxSpeed;
-            }
-
-            // Torque to apply to the wheels
-            Vector3 wheelRotation = new Vector3(WheelSpeed * direction, 0, 0);
-
-            // Animate the wheels turning
-            foreach (GameObject wheel in wheels) {
-                wheel.transform.Rotate(wheelRotation);
+                wheel.collider.motorTorque = motorTorque;
+                wheel.renderer.transform.Rotate(rotation);
             }
         }
     }
+}
+
+
+[System.Serializable]
+public class Wheel
+{
+    public WheelCollider collider;
+    public MeshRenderer renderer;
 }
