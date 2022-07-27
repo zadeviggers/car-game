26e6ab4a22940757ab1696af33106d0989ba32fa
Zade Viggers <zade.viggers@gmail.com>
2022-07-27 12:56:20 +1200

    Add wheel turning animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,28 +5,50 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Constants
-    private const float MaxSpeed = 30.0f, Acceleration = 100.5f;
+    private const float MaxSpeed = 30.0f, Acceleration = 100.5f, WheelTorque = 5.0f;
 
     // Components
     private Rigidbody rb;
+    private GameObject[] wheels;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        wheels = GameObject.FindGameObjectsWithTag("Wheel");
     }
 
     // Update is called once per frame
     void Update()
     {
+        // --------------
+        // Movement
+        // --------------
+
+        // Left or right movement
         float horizontalInput = Input.GetAxis("Horizontal");
+
+        // If there is any input...
         if (horizontalInput != 0)
         {
+            // This will be multiplied by the acceleration to change the direction
             int direction = 1;
+            // If the movement is left, make the multiplier negative
             if (horizontalInput < 0) direction = -1;
             
-            Vector3 force = new Vector3(Acceleration * direction, 0);
-            rb.AddForce(force, ForceMode.Force);
+            // Create a force vector for the acceleration
+            Vector3 movementForce = new Vector3(Acceleration * direction, 1);
+
+            // Apply the force
+            rb.AddForce(movementForce, ForceMode.Force);
+
+            // Torque to apply to the wheels
+            Vector3 wheelRotation = new Vector3(WheelTorque * direction, 0, 0);
+
+            // Animate the wheels turning
+            foreach (GameObject wheel in wheels) {
+                wheel.transform.Rotate(wheelRotation);
+            }
         }
     }
 }
